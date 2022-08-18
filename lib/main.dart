1a0be90b23a86c0fd508d68b79e68b518d4f0a32
Matthew Hartstonge <matt@mykro.co.nz>
2022-08-18 23:21:50 +1200

    :recycle: lib/main: migrates from `models/app_state` to `providers/app_state`.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sidelineref/screens/home_screen.dart';
 
-import 'models/app_state.dart';
+import 'providers/app_state.dart';
 
 void main() {
   runApp(
