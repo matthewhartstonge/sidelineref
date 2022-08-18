bd7478271dfddbd09b6354a864a69da27ed460e0
Matthew Hartstonge <matt@mykro.co.nz>
2022-08-18 22:12:24 +1200

    :sparkles: lib/components/quarter_timing_card: implements timer.

--- lib/components/quarter_timing_card.dart
+++ lib/components/quarter_timing_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
-class QuarterTimingCard extends StatelessWidget {
+class QuarterTimingCard extends StatefulWidget {
   const QuarterTimingCard({
     Key? key,
     required this.label,
@@ -9,20 +10,63 @@ class QuarterTimingCard extends StatelessWidget {
   final String label;
 
   @override
+  State<QuarterTimingCard> createState() => _QuarterTimingCardState();
+}
+
+class _QuarterTimingCardState extends State<QuarterTimingCard> {
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer();
+  bool isRunning = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _stopWatchTimer.dispose(); // Need to call dispose function.
+  }
+
+  void toggleTimer() {
+    if (!isRunning) {
+      _stopWatchTimer.onExecute.add(StopWatchExecute.start);
+      setState(() {
+        isRunning = true;
+      });
+    } else {
+      _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+      setState(() {
+        isRunning = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Text(label),
-          Text("00:69"),
-          IconButton(
-            onPressed: () {
-              print("button pressed");
-            },
-            icon: Icon(Icons.play_circle_outline),
+    return StreamBuilder<int>(
+      stream: _stopWatchTimer.rawTime,
+      initialData: 0,
+      builder: (context, snap) {
+        final value = snap.data as int;
+        final displayTime = StopWatchTimer.getDisplayTime(value);
+        return Card(
+          child: Column(
+            children: [
+              Text(widget.label),
+              Text(displayTime),
+              IconButton(
+                onPressed: () => toggleTimer(),
+                icon: Icon(
+                  isRunning
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outline,
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
