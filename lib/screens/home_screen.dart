e911f8894b9e6f1ab7ff950f3a81818b5c504861
Matthew Hartstonge <matt@mykro.co.nz>
2022-08-18 23:21:21 +1200

    :sparkles: lib/screens/home_screen: implements timer and point management.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sidelineref/components/quarter_timing_card.dart';
 import 'package:sidelineref/components/score_card.dart';
-import 'package:sidelineref/models/app_state.dart';
+import 'package:sidelineref/providers/app_state.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,6 +13,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final StopWatchTimer _stopWatchTimerQ1 = StopWatchTimer();
+  bool isRunningQ1 = false;
+  final StopWatchTimer _stopWatchTimerQ2 = StopWatchTimer();
+  bool isRunningQ2 = false;
+  final StopWatchTimer _stopWatchTimerQ3 = StopWatchTimer();
+  bool isRunningQ3 = false;
+  final StopWatchTimer _stopWatchTimerQ4 = StopWatchTimer();
+  bool isRunningQ4 = false;
+
+  void toggleStopwatch(StopWatchTimer sw, bool isRunning) {
+    if (!isRunning) {
+      sw.onExecute.add(StopWatchExecute.start);
+    } else {
+      sw.onExecute.add(StopWatchExecute.stop);
+    }
+  }
+
+  void resetTimers() {
+    _stopWatchTimerQ1.onExecute.add(StopWatchExecute.reset);
+    _stopWatchTimerQ2.onExecute.add(StopWatchExecute.reset);
+    _stopWatchTimerQ3.onExecute.add(StopWatchExecute.reset);
+    _stopWatchTimerQ4.onExecute.add(StopWatchExecute.reset);
+    setState(() {
+      isRunningQ1 = false;
+      isRunningQ2 = false;
+      isRunningQ3 = false;
+      isRunningQ4 = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,11 +64,27 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: QuarterTimingCard(
                     label: "Q1",
+                    stopwatch: _stopWatchTimerQ1,
+                    isRunning: isRunningQ1,
+                    onPlayToggled: () {
+                      toggleStopwatch(_stopWatchTimerQ1, isRunningQ1);
+                      setState(() {
+                        isRunningQ1 = !isRunningQ1;
+                      });
+                    },
                   ),
                 ),
                 Expanded(
                   child: QuarterTimingCard(
                     label: "Q2",
+                    stopwatch: _stopWatchTimerQ2,
+                    isRunning: isRunningQ2,
+                    onPlayToggled: () {
+                      toggleStopwatch(_stopWatchTimerQ2, isRunningQ2);
+                      setState(() {
+                        isRunningQ2 = !isRunningQ2;
+                      });
+                    },
                   ),
                 ),
               ],
@@ -46,11 +94,27 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: QuarterTimingCard(
                     label: "Q3",
+                    stopwatch: _stopWatchTimerQ3,
+                    isRunning: isRunningQ3,
+                    onPlayToggled: () {
+                      toggleStopwatch(_stopWatchTimerQ3, isRunningQ3);
+                      setState(() {
+                        isRunningQ3 = !isRunningQ3;
+                      });
+                    },
                   ),
                 ),
                 Expanded(
                   child: QuarterTimingCard(
                     label: "Q4",
+                    stopwatch: _stopWatchTimerQ4,
+                    isRunning: isRunningQ4,
+                    onPlayToggled: () {
+                      toggleStopwatch(_stopWatchTimerQ4, isRunningQ4);
+                      setState(() {
+                        isRunningQ4 = !isRunningQ4;
+                      });
+                    },
                   ),
                 ),
               ],
@@ -74,6 +138,54 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ],
             ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            child: Text("Reset Timers"),
+                            onPressed: () {
+                              resetTimers();
+                            },
+                          ),
+                        ),
+                        Container(width: 8.0),
+                        Expanded(
+                          child: Consumer<AppState>(
+                            builder: (context, appState, child) =>
+                                ElevatedButton(
+                              child: Text("Reset Points"),
+                              onPressed: () {
+                                appState.resetPoints();
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<AppState>(
+                  builder: (context, appState, child) => ElevatedButton(
+                    child: Text("Reset All"),
+                    onPressed: () {
+                      resetTimers();
+                      appState.resetPoints();
+                    },
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
